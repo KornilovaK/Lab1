c97e13fdaebfe79a183b60a2c55e997aa1b8e80d
KornilovaK <Katya.kornil10@gmail.com>
2025-02-22 12:09:56 +0300

    Initial commit: CMake project setup

---
+++ build/CMakeFiles/Lab1.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Lab1.dir/src/main.cpp.o"
+  "CMakeFiles/Lab1.dir/src/main.cpp.o.d"
+  "CMakeFiles/Lab1.dir/src/utils.cpp.o"
+  "CMakeFiles/Lab1.dir/src/utils.cpp.o.d"
+  "Lab1"
+  "Lab1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Lab1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/Lab1.dir/src/utils.cpp.o.d
@@ -0,0 +1,3 @@
+CMakeFiles/Lab1.dir/src/utils.cpp.o: \
+ /home/ekaterina/projects/Lab1/src/utils.cpp /usr/include/stdc-predef.h \
+ /home/ekaterina/projects/Lab1/src/utils.h
